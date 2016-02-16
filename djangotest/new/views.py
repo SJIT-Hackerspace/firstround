36dd6752c45b6505423b744610cde61dcb0ac544
Prawin <theincredible13@gmail.com>
2016-02-16 11:29:44 +0530

    testcasesupdated

--- djangotest/new/views.py
+++ djangotest/new/views.py
@@ -27,34 +27,35 @@ def get(request):
 	source = requestdict["source"]
 	lang = requestdict["lang"]
 	testcases = requestdict["testcases"]
-	output = requestdict["out"]
+	output=requestdict.get("output")
 	timeout=1
+	
 	url = "api.hackerrank.com/checker/submission.json"
 	api_key = "hackerrank|161256-622|faa76a548e2dce2ef37df6a68d4dc0c75bd760f3"
 	r = requests.post("http://api.hackerrank.com/checker/submission.json", data = {
 	    "source" : source,
 	    "lang" : lang,
-	    "testcases" : json.dumps([testcases]),
+	    "testcases" : testcases,
 	    "api_key" : api_key
 	})
 	result = r.json()
-	#if(result['result']['stdout'][0]!="")
+	
+	out=int(result['result']['stdout'][0])
+
+	#out=int(out)
 	try:
-		out=int(result['result']['stdout'][0][0])
-	#out=out.replace(" ","")
-	#std="["+'"'+out+'"'+"]"
-	#std=std.replace(" ","")
-		#testcases=int(testcases)
+
 		output=int(output)
-		
-		if(output==out and int(result['result']['time'][0])<timeout):
+		#return HttpResponse(out)	
+		if(output==out):
+			#return HttpResponse(output)	
 			return HttpResponse("Output: "+result['result']['stdout'][0]+ "<br>" +"Compile Time: "+str(result['result']['time'][0])	)
 		else:
-			return HttpResponse("Expected Output :"+output+"<br>"+"Output:"+result['result']['stdout'][0])
-
+			return HttpResponse("Expected Output :"+str(output)+"<br>"+"Output:"+(result['result']['stdout'][0]))
+		#return HttpResponse(out)	
 	except TypeError:	
-		
-		return HttpResponse("Compile error:<br>"+result['result']['compilemessage'])
+		return HttpResponse(output)	
+		#return HttpResponse("Compile error:<br>"+result['result']['compilemessage'])
 	except:
 		return HttpResponse(output)	
 
